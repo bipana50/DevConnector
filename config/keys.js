deb6f7d2aa60ea94654c4278d5608048b46e2acc
Bipana Shrestha <bipanashrestha50@gmail.com>
2020-11-17 10:37:36 +0545

    devconnector backend all and frontend layout

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
     "mongodb+srv://bipana1234:bipana1234@bipanadb.b50fa.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  secretOrKey: "bipanasecret",
 };
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const bodyParser = require("body-parser");
+const passport = require("passport");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+//Body-Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB config
 const db = require("./config/keys").mongoURI;
@@ -16,7 +21,11 @@ mongoose
   .then(() => console.log("Mongodb connected"))
   .catch((e) => console.log(e));
 
-app.get("/", (req, res) => res.send("hello world"));
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport Config
+require("./config/passport")(passport);
 
 //Use Routes
 app.use("/api/users", users);
